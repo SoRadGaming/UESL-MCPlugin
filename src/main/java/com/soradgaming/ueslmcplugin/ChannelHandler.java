fc2e7bf1be013546cb13bbb15efea4f1c46580c1
SoRadGaming <47128515+SoRadGaming@users.noreply.github.com>
2021-01-18 14:56:58 +1100

    Update 1.1, Added world change support

--- src/main/java/com/soradgaming/ueslmcplugin/ChannelHandler.java
+++ src/main/java/com/soradgaming/ueslmcplugin/ChannelHandler.java
@@ -2,7 +2,6 @@ package com.soradgaming.ueslmcplugin;
 
 import net.raidstone.wgevents.events.RegionEnteredEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,36 +11,52 @@ import java.lang.String;
 
 public class ChannelHandler extends JavaPlugin implements Listener {
 
-    @Override
-    public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
-        for(Player p : Bukkit.getOnlinePlayers())
-        {
-                p.sendMessage("Test");
+    @EventHandler
+    public void onRegionEntered(RegionEnteredEvent event) throws InterruptedException {
+        final Player player = event.getPlayer();
+        final String name = player.getDisplayName();
+        final String regionName = event.getRegionName();
+
+        changeChannel(name,regionName);
+        player.sendMessage("Test Message");
+
+    }
+
+    @EventHandler
+    public void onWorldChange(PlayerChangedWorldEvent event) throws InterruptedException {
+        final Player player = event.getPlayer();
+        final String name = player.getDisplayName();
+        final String world = player.getWorld().toString();
+        final String from = event.getFrom().toString();
+
+        if (world.startsWith("factions")) {
+            changeChannel(name, "factions");
+        }
+        else if (world.equals("Creative")) {
+            changeChannel(name, "creative");
+        }
+        else if (world.startsWith("survival")) {
+            changeChannel(name, "survival");
+        }
+        else if (world.equals("Breeze2")) {
+            changeChannel(name, "Breeze2");
+        }
+        else if (world.startsWith("IridiumSkyblock")) {
+            changeChannel(name, "IridiumSkyblock");
+        }
+        else if (world.equals("PlanetParkour")) {
+            changeChannel(name, "PlanetParkour");
+        }
+        else if (world.equals("ParkourParadise")) {
+            changeChannel(name, "ParkourParadise");
         }
     }
 
-    @EventHandler
-    public void onRegionEntered(RegionEnteredEvent event)
-    {
-        Player player = Bukkit.getPlayer(event.getUUID());
-        if (player == null) return;
-
-        String regionName = event.getRegionName();
-        player.sendMessage("Test Message");
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " permission set ultrachat.channel true");
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "channel " + regionName);
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " permission set ultrachat.channel false");
-
-    }
-
-    @EventHandler
-    public void onWorldChange(PlayerChangedWorldEvent event){
-
-        Player player = event.getPlayer();
-        World world = player.getWorld();
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " permission set ultrachat.channel true");
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "channel " + world);
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " permission set ultrachat.channel false");
+    public void changeChannel(String player, String channel) throws InterruptedException {
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player + " permission set ultrachat.channel true");
+        wait(1);
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "channel " + channel);
+        wait(1);
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player + " permission set ultrachat.channel false");
     }
 }
