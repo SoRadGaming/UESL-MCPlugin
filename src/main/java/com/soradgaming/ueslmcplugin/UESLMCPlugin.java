50857796ea541aa765ecc951cfae501338bd351a
SoRadGaming <47128515+SoRadGaming@users.noreply.github.com>
2021-01-18 16:02:28 +1100

    readded main method

---
+++ src/main/java/com/soradgaming/ueslmcplugin/UESLMCPlugin.java
@@ -0,0 +1,17 @@
+package com.soradgaming.ueslmcplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UESLMCPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin starup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
