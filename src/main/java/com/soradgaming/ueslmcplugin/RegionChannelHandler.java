e386b30361e62b78b2ff7cae66f8c8e15482d7c9
SoRadGaming <47128515+SoRadGaming@users.noreply.github.com>
2021-01-19 12:41:50 +1100

    v1.4 - updated dependecy - new ultrachat canle change system removeing need for perms - new version update 1.4

--- src/main/java/com/soradgaming/ueslmcplugin/RegionChannelHandler.java
+++ src/main/java/com/soradgaming/ueslmcplugin/RegionChannelHandler.java
@@ -1,15 +1,20 @@
 package com.soradgaming.ueslmcplugin;
 
+import me.ryandw11.ultrachat.api.UltraChatAPI;
+import me.ryandw11.ultrachat.api.channels.ChatChannel;
 import net.raidstone.wgevents.events.RegionEnteredEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import java.lang.String;
+import java.nio.channels.Channel;
 
 public class RegionChannelHandler implements Listener {
+    public static UltraChatAPI chat = new UltraChatAPI();
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onRegionEntered(RegionEnteredEvent event) {
         Player player = event.getPlayer();
         assert player != null;
@@ -18,26 +23,13 @@ public class RegionChannelHandler implements Listener {
 
         if(!regionName.equals("factions_spawn")) {
             changeChannel(name, regionName);
-            changeChannel(name, regionName);
         }
     }
 
     public static void changeChannel(String name, String channel) {
         Player player = Bukkit.getServer().getPlayer(name);
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " permission set ultrachat.channel true");
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        assert player != null;
-        player.performCommand("channel " + channel);
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " permission set ultrachat.channel false");
+        Channel c = (Channel) chat.getChannelManager().getChannelByName(channel);
+        chat.getChannelManager().setPlayerChannel(player, (ChatChannel) c);
     }
 }
 
--- src/main/java/com/soradgaming/ueslmcplugin/WorldChannelHandler.java
+++ src/main/java/com/soradgaming/ueslmcplugin/WorldChannelHandler.java
@@ -1,14 +1,20 @@
 package com.soradgaming.ueslmcplugin;
 
+import me.ryandw11.ultrachat.api.UltraChatAPI;
+import me.ryandw11.ultrachat.api.channels.ChatChannel;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 
-public class WorldChannelHandler implements Listener {
+import java.nio.channels.Channel;
 
-    @EventHandler
+public class WorldChannelHandler implements Listener {
+    public static UltraChatAPI chat = new UltraChatAPI();
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onWorldChange(PlayerChangedWorldEvent event) {
         Player player = event.getPlayer();
         String name = player.getName();
@@ -17,7 +23,6 @@ public class WorldChannelHandler implements Listener {
 
         if (worldTo.startsWith("factions") && !worldFrom.startsWith("factions")) {
             changeChannel(name, "factions");
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " permission set ultrachat.chat.factions true");
         } else if (worldTo.equals("Creative")) {
             changeChannel(name, "creative");
         } else if (worldTo.startsWith("survival") && !worldFrom.startsWith("survival")) {
@@ -34,7 +39,7 @@ public class WorldChannelHandler implements Listener {
             return;
         }
     }
-
+    /*
     public static void changeChannel(String name, String channel) {
         Player player = Bukkit.getServer().getPlayer(name);
         Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " permission set ultrachat.channel true");
@@ -52,4 +57,12 @@ public class WorldChannelHandler implements Listener {
         }
         Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + name + " permission set ultrachat.channel false");
     }
+    */
+
+    public static void changeChannel(String name, String channel) {
+        Player player = Bukkit.getServer().getPlayer(name);
+        Channel c = (Channel) chat.getChannelManager().getChannelByName(channel);
+        chat.getChannelManager().setPlayerChannel(player, (ChatChannel) c);
+    }
+
 }
