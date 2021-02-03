8e1b9791c8d8c131110977e7edfe80aa884b6402
SoRadGaming <47128515+SoRadGaming@users.noreply.github.com>
2021-02-03 19:24:37 +1100

    Added Custom Event to Run when Enter Region Event is called (from CE plugin port)

--- src/main/java/com/soradgaming/ueslmcplugin/Handler/RegionChannel.java
+++ src/main/java/com/soradgaming/ueslmcplugin/Handler/RegionChannel.java
@@ -3,6 +3,8 @@ package com.soradgaming.ueslmcplugin.Handler;
 import me.ryandw11.ultrachat.api.UltraChatAPI;
 import me.ryandw11.ultrachat.api.channels.ChatChannel;
 import net.raidstone.wgevents.events.RegionEnteredEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,6 +18,7 @@ public class RegionChannel implements Listener {
         Player player = event.getPlayer();
         String regionName = event.getRegionName();
 
+        //Channel Event
         if (regionName.equals("hub")) {
             changeChannel(player, regionName);
         } else if (regionName.equals("minigames")) {
@@ -34,6 +37,33 @@ public class RegionChannel implements Listener {
             changeChannel(player, regionName);
         } else if (regionName.equals("paintball")) {
             changeChannel(player, regionName);
+        }
+
+        //Custom Events
+
+        //Skyblock_Portal
+        if (regionName.equals("skyblock_portal_1") || regionName.equals("skyblock_portal_2") || regionName.equals("skyblock_portal_3") || regionName.equals("skyblock_portal_4") || regionName.equals("skyblock_portal_5") || regionName.equals("skyblock_portal_6")) {
+            player.performCommand("is home");
+            player.sendMessage("Welcome to Skyblocks!");
+        }
+        //Skyblock_Death_Loop_Fix
+        if (regionName.equals("skyblock_death_box")) {
+            player.performCommand("is home");
+        }
+        //Dues Gamemode
+        if (regionName.equals("duels_arena")) {
+            player.setGameMode(GameMode.ADVENTURE);
+        }
+        //SecretHub Parkour Finish
+        if (regionName.equals("shub_portal")) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player + " permission set essentials.warps.secrethub true");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "title" + player + " title {\"text\":\"Congratulations\", \"bold\":true, \"italic\":false, \"color\":\"blue\"}");
+            player.sendMessage("Use /shub to get here in the future");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "warp secrethub " + player);
+        }
+        //ParkourParadise Portal
+        if (regionName.equals("ParkourParadise_Portal")) {
+            player.performCommand("pa join parkourparadise");
         }
     }
 
