0038506f79babd1427ddc001ed17984891110403
SoRadGaming <47128515+SoRadGaming@users.noreply.github.com>
2021-02-06 21:49:34 +1100

    New Commands System /uesl /uesl reload (does all) /uesl data edit player dataset True/False

--- src/main/java/com/soradgaming/ueslmcplugin/Commands/UESLCommand.java
+++ src/main/java/com/soradgaming/ueslmcplugin/Commands/UESLCommand.java
@@ -2,10 +2,12 @@ package com.soradgaming.ueslmcplugin.Commands;
 
 import com.soradgaming.ueslmcplugin.UESLMCPlugin;
 import me.ryandw11.ultrachat.util.ChatUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class UESLCommand implements CommandExecutor {
@@ -23,12 +25,12 @@ public class UESLCommand implements CommandExecutor {
             sender.sendMessage(ChatColor.BLUE + "Plugin developed by:" + ChatUtil.translateColorCode("#1782FE") + " SoRadGaming");
             sender.sendMessage(ChatColor.BLUE + "Version: " + ChatUtil.translateColorCode("#1782FE") + String.format("%s", plugin.getDescription().getVersion()));
             sender.sendMessage(ChatColor.BLUE + "Plugin:" + ChatUtil.translateColorCode("#1782FE") + " https://github.com/SoRadGaming/UESL-MCPlugin");
-            sender.sendMessage(ChatColor.BLUE + "Do " + ChatUtil.translateColorCode("#1782FE") + " /uesl help " + ChatColor.BLUE + "for the list of commands!");
+            sender.sendMessage(ChatColor.BLUE + "Do " + ChatUtil.translateColorCode("#1782FE") + "/uesl help " + ChatColor.BLUE + "for the list of commands!");
             sender.sendMessage(ChatColor.BLUE + "=============={" + ChatUtil.translateColorCode("#1782FE") + "UESL-MCPlugin" + ChatColor.BLUE + "}==============");
 
         } else if (args.length == 1 && args[0].equalsIgnoreCase("help")) {
             if (!sender.isOp()) {
-                sender.sendMessage("§4[ERROR] You don't have permission to do that");
+                sender.sendMessage(ChatColor.RED + "You don't have permission to do that");
                 return true;
             }
             sender.sendMessage(ChatColor.BLUE + "-----------------=[" + ChatUtil.translateColorCode("#1782FE") + "UESL-MCPlugin" + ChatColor.BLUE + "]=-----------------");
@@ -39,30 +41,33 @@ public class UESLCommand implements CommandExecutor {
 
         } else if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
             if (sender.isOp()) {
-                sender.sendMessage("§4[ERROR] Undefined Variable");
+                plugin.saveFile();
+                sender.sendMessage("[UESL-MCPlugin] Data Reloaded");
+                plugin.getLogger().info("Data Reloaded");
+                plugin.reloadConfig();
+                plugin.loadFile();
+                sender.sendMessage("[UESL-MCPlugin] Config Reloaded");
+                plugin.getLogger().info("Config Reloaded");
             } else {
-                sender.sendMessage("§4[ERROR] You don't have permission to do that");
+                sender.sendMessage(ChatColor.RED + "You don't have permission to do that");
             }
-        } else if (args.length == 2 && args[0].equalsIgnoreCase("reload")) {
-            if (args[1].equals("data")) {
-                if (sender.isOp()) {
-                    plugin.saveFile();
-                    plugin.loadFile();
-                    sender.sendMessage("[UESL-MCPlugin] Data Reloaded");
-                    plugin.getLogger().info("[UESL-MCPlugin] Data Reloaded");
-                } else {
-                    sender.sendMessage("§4[ERROR] You don't have permission to do that");
+        } else if (args.length == 5 && args[0].equalsIgnoreCase("data")) {
+            if (sender.isOp()) {
+                if (args[1].equals("edit")) {
+                    Player player = Bukkit.getServer().getPlayer(args[2]);
+                    boolean TorF = Boolean.parseBoolean(args[4]);
+
+                    if (player == null) {
+                        sender.sendMessage(ChatColor.RED + "Player can not be null!");
+                        return true;
+                    }
+
+                    if (args[3].equals("planetparkour_completed") || args[3].equals("parkourparadise_completed")) {
+                        plugin.data.set(player.getUniqueId().toString() + "." + args[3], TorF);
+                        plugin.saveFile();
+                    } else sender.sendMessage(ChatColor.RED + "Invalid Data Set");
                 }
-            }  else if (args[1].equals("config")) {
-                if (sender.isOp()) {
-                    plugin.reloadConfig();
-                    plugin.loadFile();
-                    sender.sendMessage("[UESL-MCPlugin] Config Reloaded");
-                    plugin.getLogger().info("[UESL-MCPlugin] Config Reloaded");
-                } else {
-                    sender.sendMessage("§4[ERROR] You don't have permission to do that");
-                }
-            }
+            } else sender.sendMessage(ChatColor.RED + "You don't have permission to do that");
         }
         return false;
     }
