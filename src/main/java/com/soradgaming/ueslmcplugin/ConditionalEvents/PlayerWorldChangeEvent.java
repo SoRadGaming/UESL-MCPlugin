de836f6092925546c8724db833bfb0d0b2c73365
SoRadGaming <47128515+SoRadGaming@users.noreply.github.com>
2021-02-06 13:50:41 +1100

    Fixed Parkour Join message conditions

--- src/main/java/com/soradgaming/ueslmcplugin/ConditionalEvents/PlayerWorldChangeEvent.java
+++ src/main/java/com/soradgaming/ueslmcplugin/ConditionalEvents/PlayerWorldChangeEvent.java
@@ -5,10 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
-import java.util.Collections;
 import java.util.Objects;
 
 public class PlayerWorldChangeEvent implements Listener {
@@ -25,20 +22,17 @@ public class PlayerWorldChangeEvent implements Listener {
         String worldFrom = event.getFrom().getName();
         String worldTo = player.getWorld().getName();
 
-        //Half Heart PlanetParkour Fix
-        if (worldFrom.equals("PlanetParkour")) {
-                player.setHealth(20);
-                player.addPotionEffects(Collections.singleton(new PotionEffect(PotionEffectType.REGENERATION, 1, 20)));
-        }
-
         //Parkour Join
         if (worldFrom.equals("World")) {
             if (worldTo.equals("ParkourParadise") && plugin.data.getBoolean(Objects.requireNonNull(player).getUniqueId().toString() + ".parkourparadise_completed")) {
-                    player.sendMessage("You have already claimed the prize for this course!");
+                player.sendMessage("You have already claimed the prize for this course!");
+                player.sendMessage("Use /minigames to leave the course and return to minigames");
             } else if (worldTo.equals("PlanetParkour") && plugin.data.getBoolean(Objects.requireNonNull(player).getUniqueId().toString() + ".planetparkour_completed")) {
-                    player.sendMessage("You have already claimed the prize for this course!");
+                player.sendMessage("You have already claimed the prize for this course!");
+                player.sendMessage("Use /minigames to leave the course and return to minigames");
+            } else if (worldTo.equals("PlanetParkour") || worldTo.equals("ParkourParadise")) {
+                player.sendMessage("Use /minigames to leave the course and return to minigames");
             }
-            player.sendMessage("Use /minigames to leave the course and return to minigames");
         }
     }
 }
